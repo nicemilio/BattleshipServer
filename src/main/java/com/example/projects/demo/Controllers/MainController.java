ecf6cd6554d32b450ea6b9f8f62b605d8d5ff51d
Lachlan McDowall <lachlan.mcdowall@gmail.com>
2022-02-13 17:26:25 +0100

    message

--- src/main/java/com/example/projects/demo/Controllers/MainController.java
+++ src/main/java/com/example/projects/demo/Controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.example.projects.demo.Controllers;
 
-import com.example.projects.demo.Services.MainService;
+
+import com.example.projects.demo.Services.SetupService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @Autowired
-    MainService service;
+    SetupService service;
 
 
-    @GetMapping ("/hello")
-    public String hello (Model model){
+    @GetMapping(value={"/", "/setup"})
+    public String setup(Model model){
         model.addAttribute("name", service.getName());
         return "beliebig/hello";
     }
--- src/main/java/com/example/projects/demo/Services/SetupService.java
+++ src/main/java/com/example/projects/demo/Services/SetupService.java
@@ -3,7 +3,7 @@ package com.example.projects.demo.Services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MainService {
+public class SetupService {
     public String getName (){
         return "Lachlan McDowall";
     }
